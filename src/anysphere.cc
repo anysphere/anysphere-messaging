b19d71d26f5d5af25ca491cc55728f7ae0472e4d
Sualeh Asif <sualehasif996@gmail.com>
2021-12-30 18:04:12 -0500

    aaaaa builds are working but I need to make one big WORKSPACE file

--- src/anysphere.cc
+++ src/anysphere.cc
@@ -1,6 +1,16 @@
+#include <grpcpp/grpcpp.h>
+
 #include <ctime>
 #include <iostream>
 #include <string>
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+
+using messenger::Messenger;
+using messenger::RegisterInfo;
+using messenger::RegisterResponse;
 
 std::string get_greet(const std::string& who) { return "Hello " + who; }
 
@@ -9,12 +19,46 @@ void print_localtime() {
   std::cout << std::asctime(std::localtime(&result));
 }
 
-int main(int argc, char** argv) {
-  std::string who = "world";
-  if (argc > 1) {
-    who = argv[1];
+void test(std::shared_ptr<Channel> channel,
+          std::unique_ptr<Greeter::Stub> stub) {
+  // Data we are sending to the server.
+  RegisterInfo request;
+  request.set_publicKey("hi_i_am_public_key");
+
+  // Container for the data we expect from the server.
+  RegisterResponse reply;
+
+  // Context for the client. It could be used to convey extra information to
+  // the server and/or tweak certain RPC behaviors.
+  ClientContext context;
+
+  // The actual RPC.
+  Status status = stub->Register(&context, request, &reply);
+
+  // Act upon its status.
+  if (status.ok()) {
+    return reply.message();
+  } else {
+    std::cout << status.error_code() << ": " << status.error_message()
+              << std::endl;
+    return "RPC failed";
   }
-  std::cout << get_greet(who) << std::endl;
-  print_localtime();
+}
+
+int main(int argc, char** argv) {
+  std::string server_address("0.0.0.0:50051");
+  if (argc > 1) {
+    server_address = argv[1];
+  }
+  std::cout << "Client querying server address: " << server_address
+            << std::endl;
+
+  std::shared_ptr<Channel> channel =
+      grpc::CreateChannel(server_address, grpc::InsecureChannelCredentials());
+
+  std::unique_ptr<Greeter::Stub> stub = Greeter::NewStub(channel);
+
+  test(channel, stub);
+
   return 0;
 }
