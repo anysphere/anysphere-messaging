d036aacde675512ecf0ea45b2bc74975cd3e16da
Sualeh Asif <sualehasif996@gmail.com>
2022-01-04 23:15:17 +0000

    refactor part 1

---
+++ asphr/BUILD
@@ -0,0 +1,15 @@
+cc_library(
+    name = "asphr_lib",
+    srcs = [
+        "asphr.h",
+        "base64.h",
+        "nlohmann_json.h",
+    ],
+    visibility = ["//visibility:public"],
+    deps = [
+        "@com_google_absl//absl/hash",
+        "@com_google_absl//absl/random",
+        "@com_google_absl//absl/strings",
+        "@com_google_absl//absl/time",
+    ],
+)
---
+++ asphr/asphr.h
@@ -0,0 +1,28 @@
+#include <ctime>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <unordered_map>
+#include <vector>
+
+#include "absl/hash/hash.h"
+#include "absl/random/random.h"
+#include "absl/strings/str_cat.h"
+#include "absl/time/clock.h"
+#include "absl/time/time.h"
+#include "base64.h"
+#include "nlohmann_json.h"
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::to_string;
+
+using std::vector;
+
+using json = nlohmann::json;
+
+using absl::StrCat;
+using absl::Time;
