c6aaebc83796e42c3f88fb4e51e273a5c241a44e
Sualeh Asif <sualehasif996@gmail.com>
2022-01-05 02:08:01 +0000

    adding asphr/pir.

---
+++ asphr/pir/fast_pir/fast_pir.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <assert.h>
+#include <seal/seal.h>
+
+#include <array>
+#include <string>
+#include <vector>
+
+#include "asphr/asphr.h"
+#include "fast_pir_config.h"
+
+template <typename Ciphertext_t, typename GaloisKeys_t>
+struct FastPIRQuery {
+  // TODO: STORE GALOIS KEYS IN POSTGRES, DO NOT SEND IN EVERY QUERY BECAUSE
+  // THEY DO NOT CHANGE
+  vector<Ciphertext_t> query;
+  GaloisKeys_t galois_keys;
+
+  auto serialize_to_string() noexcept(false) -> string {
+    std::stringstream s_stream;
+    galois_keys.save(s_stream);
+    for (const auto &c : query) {
+      c.save(s_stream);
+    }
+    return s_stream.str();
+  }
+
+  // throws if deserialization fails
+  auto deserialize_from_string(const string &s,
+                               seal::SEALContext sc) noexcept(false) -> void {
+    auto s_stream = std::stringstream(s);
+    size_t position = 0;
+    position += galois_keys.load(sc, s_stream);
+    while (position < s.size()) {
+      seal::Ciphertext c;
+      position += c.load(sc, s_stream);
+      query.push_back(c);
+    }
+  }
+};
+
+struct FastPIRAnswer {
+  seal::Ciphertext answer;
+
+  // throws if serialization fails
+  auto serialize_to_string() noexcept(false) -> string {
+    std::stringstream s_stream;
+    answer.save(s_stream);
+    return s_stream.str();
+  }
+  // throws if deserialization fails
+  auto deserialize_from_string(const string &s,
+                               seal::SEALContext sc) noexcept(false) -> void {
+    auto s_stream = std::stringstream(s);
+    answer.load(sc, s_stream);
+  }
+};
---
+++ asphr/pir/fast_pir/fast_pir_config.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <array>
+#include <bit>
+#include <cstddef>
+#include <cstdint>
+#include <vector>
+
+// these parameters are taken from
+// https://github.com/ishtiyaque/FastPIR/blob/master/src/bfvparams.h
+// TODO: optimize them
+
+#define CEIL_DIV(a, b) (((a) + (b)-1) / (b))
+
+using std::array;
+using std::size_t;
+using std::vector;
+
+constexpr size_t POLY_MODULUS_DEGREE = 4096;
+static_assert(std::popcount(POLY_MODULUS_DEGREE) == 1,
+              "POLY_MODULUS_DEGREE must be a power of 2");
+
+constexpr uint64_t PRIME_54 = 18'014'398'509'309'953ULL;
+// static_assert(is_prime(PRIME_54), "PRIME_54 must be prime"); // seems
+// unnecessary to code this up just to check this. VERIFY MANUALLY.
+
+constexpr uint64_t PRIME_55 = 36'028'797'018'652'673ULL;
+// static_assert(is_prime(PRIME_55), "PRIME_55 must be prime"); // seems
+// unnecessary to code this up just to check this. VERIFY MANUALLY.
+
+constexpr array<uint64_t, 2> COEFF_MODULUS_FACTORIZATION({PRIME_54, PRIME_55});
+
+// number of bits we store per coefficient. we store a power of 2 because
+// performing modulo mod 2 is much much faster, when encoding
+constexpr uint64_t PLAIN_BITS = 18;
+
+// must be greater than 2^PLAIN_BITS
+// must also be congruent to 1 modulo 2*POLY_MODULUS_DEGREE
+// finally, must also be a prime
+constexpr uint64_t PLAIN_MODULUS = 270'337;
+static_assert(PLAIN_MODULUS >= 1ULL << PLAIN_BITS,
+              "PLAIN_MODULUS must be greater than 2^PLAIN_BITS");
+static_assert(
+    PLAIN_MODULUS % (2 * POLY_MODULUS_DEGREE) == 1,
+    "PLAIN_MODULUS must be congruent to 1 modulo 2*POLY_MODULUS_DEGREE");
+// static_assert(is_prime(PLAIN_MODULUS), "PLAIN_MODULUS must be prime"); //
+// seems unnecessary to code this up just to check this. VERIFY MANUALLY.
+
+auto create_context_params() -> seal::EncryptionParameters {
+  seal::EncryptionParameters params(seal::scheme_type::bfv);
+  params.set_poly_modulus_degree(POLY_MODULUS_DEGREE);
+  vector<seal::Modulus> coeff_modulus;
+  for (auto &prime : COEFF_MODULUS_FACTORIZATION) {
+    coeff_modulus.push_back(seal::Modulus(prime));
+  }
+  params.set_coeff_modulus(coeff_modulus);
+  params.set_plain_modulus(PLAIN_MODULUS);
+  return params;
+}
+
+constexpr size_t SEAL_DB_COLUMNS = CEIL_DIV(MESSAGE_SIZE_BITS, PLAIN_BITS);
