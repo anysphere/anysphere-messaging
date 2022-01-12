6875eb7fbfe3bcc66955b6e1b86a79ca49afa462
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2022-01-12 09:55:44 -0600

    remove extra things

--- gui/src/renderer.ts
+++ gui/src/renderer.ts
@@ -26,6 +26,8 @@
  * ```
  */
 
-import './index.css';
+import "./app.tsx";
 
-console.log('👋 This message is being logged by "renderer.js", included via webpack');
+console.log(
+  '👋 This message is being logged by "renderer.js", included via webpack'
+);
