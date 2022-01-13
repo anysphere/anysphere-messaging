4a63341c6c67cca264dc4b06302e791bf559e33c
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2022-01-12 22:28:05 -0600

    make everything work yay!

--- gui/src/preload.ts
+++ gui/src/preload.ts
@@ -1,16 +1,29 @@
-console.log("preload!");
-
-const daemonMessages = require("./daemon/schema/daemon_pb");
-const daemonService = require("./daemon/schema/daemon_grpc_pb");
-
-const registerUserRequest = new daemonMessages.RegisterUserRequest();
-registerUserRequest.setName("test");
-console.log(registerUserRequest);
-
 const { contextBridge } = require("electron");
+const { promisify } = require("util");
+var grpc = require("@grpc/grpc-js");
 
-contextBridge.exposeInMainWorld("send", (to: string, message: string) => {
-  console.log(`sending message to ${to}: ${message}`);
+const daemonM = require("./daemon/schema/daemon_pb");
+const daemonS = require("./daemon/schema/daemon_grpc_pb");
+const daemonClient = new daemonS.DaemonClient(
+  "unix:///var/run/asphr.sock",
+  grpc.credentials.createInsecure()
+);
+
+contextBridge.exposeInMainWorld("send", async (to: string, message: string) => {
+  const request = new daemonM.SendMessageRequest();
+  request.setName(to);
+  request.setMessage(message);
+  const sendMessage = promisify(daemonClient.sendMessage).bind(daemonClient);
+  try {
+    const response = await sendMessage(request);
+    if (!response.getSuccess()) {
+      console.log(`unsuccessful send`);
+      return;
+    }
+  } catch (e) {
+    console.log(`error in send: ${e}`);
+    return e;
+  }
 });
 
 contextBridge.exposeInMainWorld("getNewMessages", () => {
