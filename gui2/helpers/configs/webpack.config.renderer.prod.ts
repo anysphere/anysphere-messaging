5bbd649e2840ff4accda4d46058aa7ac416b540d
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2022-01-14 02:08:00 -0600

    remove another bad dependency

--- gui2/helpers/configs/webpack.config.renderer.prod.ts
+++ gui2/helpers/configs/webpack.config.renderer.prod.ts
@@ -2,43 +2,42 @@
  * Build config for electron renderer process
  */
 
-import path from 'path';
-import webpack from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
-import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
-import { merge } from 'webpack-merge';
-import baseConfig from './webpack.config.base';
-import webpackPaths from './webpack.paths';
-import checkNodeEnv from '../scripts/check-node-env';
-import deleteSourceMaps from '../scripts/delete-source-maps';
+import path from "path";
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
+import { merge } from "webpack-merge";
+import baseConfig from "./webpack.config.base";
+import webpackPaths from "./webpack.paths";
+import checkNodeEnv from "../scripts/check-node-env";
+import deleteSourceMaps from "../scripts/delete-source-maps";
 
-checkNodeEnv('production');
+checkNodeEnv("production");
 deleteSourceMaps();
 
 const devtoolsConfig =
-  process.env.DEBUG_PROD === 'true'
+  process.env.DEBUG_PROD === "true"
     ? {
-        devtool: 'source-map',
+        devtool: "source-map",
       }
     : {};
 
 const configuration: webpack.Configuration = {
   ...devtoolsConfig,
 
-  mode: 'production',
+  mode: "production",
 
-  target: ['web', 'electron-renderer'],
+  target: ["web", "electron-renderer"],
 
-  entry: [path.join(webpackPaths.srcRendererPath, 'index.tsx')],
+  entry: [path.join(webpackPaths.srcRendererPath, "index.tsx")],
 
   output: {
     path: webpackPaths.distRendererPath,
-    publicPath: './',
-    filename: 'renderer.js',
+    publicPath: "./",
+    filename: "renderer.js",
     library: {
-      type: 'umd',
+      type: "umd",
     },
   },
 
@@ -46,18 +45,18 @@ const configuration: webpack.Configuration = {
     rules: [
       {
         test: /\.s?(a|c)ss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
         exclude: /\.module\.s?(c|a)ss$/,
       },
       // Fonts
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       // Images
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
@@ -78,28 +77,24 @@ const configuration: webpack.Configuration = {
      * development checks
      */
     new webpack.EnvironmentPlugin({
-      NODE_ENV: 'production',
+      NODE_ENV: "production",
       DEBUG_PROD: false,
     }),
 
     new MiniCssExtractPlugin({
-      filename: 'style.css',
-    }),
-
-    new BundleAnalyzerPlugin({
-      analyzerMode: process.env.ANALYZE === 'true' ? 'server' : 'disabled',
+      filename: "style.css",
     }),
 
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: path.join(webpackPaths.srcRendererPath, 'index.ejs'),
+      filename: "index.html",
+      template: path.join(webpackPaths.srcRendererPath, "index.ejs"),
       minify: {
         collapseWhitespace: true,
         removeAttributeQuotes: true,
         removeComments: true,
       },
       isBrowser: false,
-      isDevelopment: process.env.NODE_ENV !== 'production',
+      isDevelopment: process.env.NODE_ENV !== "production",
     }),
   ],
 };
