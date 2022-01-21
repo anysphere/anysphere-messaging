4be975fccd15f6edff7e6d18d8bb7730b482cbd4
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2022-01-20 22:47:10 -0600

    move imports up bc

--- gui/src/main/preload.js
+++ gui/src/main/preload.js
@@ -1,6 +1,9 @@
 const { contextBridge } = require("electron");
 const { promisify } = require("util");
 var grpc = require("@grpc/grpc-js");
+const daemonM = require("../daemon/schema/daemon_pb");
+const daemonS = require("../daemon/schema/daemon_grpc_pb");
+const path = require("path");
 
 function get_socket_path() {
   if (process.env.XDG_RUNTIME_DIR) {
@@ -30,9 +33,6 @@ function get_socket_address() {
   return "unix://" + get_socket_path();
 }
 
-const daemonM = require("../daemon/schema/daemon_pb");
-const daemonS = require("../daemon/schema/daemon_grpc_pb");
-const path = require("path");
 const daemonClient = new daemonS.DaemonClient(
   get_socket_address(),
   grpc.credentials.createInsecure()
