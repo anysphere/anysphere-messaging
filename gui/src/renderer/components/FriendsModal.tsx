e6cb70fa77658b938c94943570d708ef1385fec5
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2022-01-31 14:30:17 -0500

    start implement friends

--- gui/src/renderer/components/FriendsModal.tsx
+++ gui/src/renderer/components/FriendsModal.tsx
@@ -2,9 +2,19 @@ import * as React from "react";
 import Modal from "./Modal";
 
 function FriendsModal({ onClose }: { onClose: () => void }) {
+  const [selected, setSelected] = React.useState<number>(0);
+
   return (
     <Modal onClose={onClose}>
-      <div>add friends</div>
+      <div className="grid">
+        <div className={`${selected === 0 ? "bg-gray-100" : ""}`}>
+          <div className="flex flex-row">
+            <div>Add friend:</div>
+            <input type="text" />
+          </div>
+        </div>
+        <div className={`${selected === 1 ? "bg-gray-100" : ""}`}>Friend 1</div>
+      </div>
     </Modal>
   );
 }
--- gui/src/renderer/components/Modal.tsx
+++ gui/src/renderer/components/Modal.tsx
@@ -12,7 +12,7 @@ function Modal({
       <div className="backdrop-blur-sm w-screen h-screen" onClick={onClose}>
         <div className="grid w-screen h-screen">
           <div
-            className="place-self-center w-96 bg-red-500"
+            className="place-self-center w-96 bg-white rounded-md p-2"
             onClick={(event) => {
               event.stopPropagation();
             }}
