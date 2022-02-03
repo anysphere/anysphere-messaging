0b7b137e2964303ffeaa50a1db3a2fb58eb6b1ec
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2022-02-03 04:18:32 +0000

    add more base64 tests

--- asphr/base64_test.cc
+++ asphr/base64_test.cc
@@ -7,3 +7,21 @@ TEST(Base64, EmptyString) {
   auto out = Base64::Encode("");
   EXPECT_EQ(out, "");
 }
+
+TEST(Base64, EncodeArbitrary) {
+  auto out = Base64::Encode("hi my name is arvid");
+  EXPECT_EQ(out, "aGkgbXkgbmFtZSBpcyBhcnZpZA==");
+}
+
+TEST(Base64, DecodeArbitrary) {
+  std::string out;
+  Base64::Decode("aGkgbXkgbmFtZSBpcyBhcnZpZA==", out);
+  EXPECT_EQ(out, "hi my name is arvid");
+}
+
+TEST(Base64, EncodeDecode) {
+  auto out = Base64::Encode("こんにちは");
+  std::string in;
+  Base64::Decode(out, in);
+  EXPECT_EQ(in, "こんにちは");
+}
