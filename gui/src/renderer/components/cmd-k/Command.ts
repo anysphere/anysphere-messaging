8084b006d2c49d933f760b2495f6ecae73a44e8b
Sualeh Asif <sualeh@mit.edu>
2022-02-06 13:48:32 -0500

    starting to add commands

---
+++ gui/src/renderer/components/cmd-k/Command.ts
@@ -0,0 +1,32 @@
+interface CommandOptions {}
+
+export class Command {
+  perform: (...args: any) => any;
+
+  constructor(command: { perform: Command["perform"] }) {
+    this.perform = () => {
+      const negate = command.perform();
+      // no need for history if non negatable   
+      if (typeof negate !== "function") return;
+      // return if no history enabled
+
+      //TODO(sualeh): do we need history.
+      //   const history = options.history;
+      //   if (!history) return;
+      // since we are performing the same action, we'll clean up the
+      // previous call to the action and create a new history record
+      //   if (this.historyItem) {
+      //     history.remove(this.historyItem);
+      //   }
+      //   this.historyItem = history.add({
+      //     perform: command.perform,
+      //     negate,
+      //   });
+
+      //   this.history = {
+      //     undo: () => history.undo(this.historyItem),
+      //     redo: () => history.redo(this.historyItem),
+      //   };
+    };
+  }
+}
