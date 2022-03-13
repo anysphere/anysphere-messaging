007e3c326f58c0fbd16fce39e3536abcc3fefce9
Sualeh Asif <sualehasif996@gmail.com>
2022-03-12 19:30:09 -0500

    Even faster testing with multiple friends also split up (#80)
    * split these tests. check data in notion.
    
    * 🔄 "split these tests. check data in notion.
    "
    Update anysphere/client commit SHA
    🔗 https://github.com/anysphere/client/commit/4b84fd8926995c624947dcc6baa34b57b90717cf
    
    * revert anysphere-pages
    
    * revert workspace
    
    * correctly revert workspace
    
    Co-authored-by: Arvid Lunnemark <arvid.lunnemark@gmail.com>

---
+++ integration_tests/multiple_friends_tests/long_messages_test.cc
@@ -0,0 +1,246 @@
+#include "../daemon_setup.hpp"
+
+using namespace asphrdaemon;
+
+/**
+ * Things to test:
+ * - multiple friends transmitting (one person sends message to n people)
+ * - multiple friends retrieval (n people send message to one person)
+ * - long messages (that are guranteed to be chunked. Bigger than
+ *   GUARANTEED_SINGLE_MESSAGE_SIZE)
+ * - multiple friends with long messages to one person
+ * - one person sends multiple messages to another person.
+ **/
+
+namespace asphr::testing {
+namespace {
+
+class MultipleFriendsTest : public DaemonRpcTest {};
+
+TEST_F(MultipleFriendsTest, SendLongMessage) {
+  ResetStub();
+
+  vector<string> names = {"user1local", "user2local", "user3local",
+                          "user4local"};
+  vector<Crypto> cryptos;
+  vector<shared_ptr<Config>> configs;
+  for (size_t i = 0; i < names.size(); i++) {
+    auto config = gen_config(string(generateTempDir()), generateTempFile());
+    configs.push_back(config);
+  }
+  vector<unique_ptr<DaemonRpc>> rpcs;
+  vector<unique_ptr<Transmitter>> ts;
+  for (size_t i = 0; i < names.size(); i++) {
+    cryptos.push_back(gen_crypto());
+    auto msgstore = gen_msgstore(configs[i]);
+    auto rpc_ptr =
+        make_unique<DaemonRpc>(cryptos[i], configs[i], stub_, msgstore);
+    rpcs.push_back(std::move(rpc_ptr));
+    auto t_ptr =
+        make_unique<Transmitter>(cryptos[i], configs[i], stub_, msgstore);
+    ts.push_back(std::move(t_ptr));
+  }
+
+  for (size_t i = 0; i < names.size(); i++) {
+    RegisterUserRequest request;
+    request.set_name(names[i]);
+    request.set_beta_key("asphr_magic");
+    RegisterUserResponse response;
+    rpcs[i]->RegisterUser(nullptr, &request, &response);
+  }
+
+  string user1_2_key;
+  string user1_3_key;
+  string user1_4_key;
+  string user2_1_key;
+  string user3_1_key;
+  string user4_1_key;
+
+  // make user 1 add users 2-4
+  {
+    GenerateFriendKeyRequest request;
+    request.set_name(names[1]);
+    GenerateFriendKeyResponse response;
+    auto status = rpcs[0]->GenerateFriendKey(nullptr, &request, &response);
+    EXPECT_TRUE(status.ok());
+    EXPECT_GT(response.key().size(), 0);
+    user1_2_key = response.key();
+  }
+
+  {
+    GenerateFriendKeyRequest request;
+    request.set_name(names[2]);
+    GenerateFriendKeyResponse response;
+    auto status = rpcs[0]->GenerateFriendKey(nullptr, &request, &response);
+    EXPECT_TRUE(status.ok());
+    EXPECT_GT(response.key().size(), 0);
+    user1_3_key = response.key();
+  }
+
+  {
+    GenerateFriendKeyRequest request;
+    request.set_name(names[3]);
+    GenerateFriendKeyResponse response;
+    auto status = rpcs[0]->GenerateFriendKey(nullptr, &request, &response);
+    EXPECT_TRUE(status.ok());
+    EXPECT_GT(response.key().size(), 0);
+    user1_4_key = response.key();
+  }
+
+  // make users 2-4 add user 1
+  {
+    GenerateFriendKeyRequest request;
+    request.set_name(names[0]);
+    GenerateFriendKeyResponse response;
+    auto status = rpcs[1]->GenerateFriendKey(nullptr, &request, &response);
+    EXPECT_TRUE(status.ok());
+    EXPECT_GT(response.key().size(), 0);
+    user2_1_key = response.key();
+  }
+
+  {
+    GenerateFriendKeyRequest request;
+    request.set_name(names[0]);
+    GenerateFriendKeyResponse response;
+    auto status = rpcs[2]->GenerateFriendKey(nullptr, &request, &response);
+    EXPECT_TRUE(status.ok());
+    EXPECT_GT(response.key().size(), 0);
+    user3_1_key = response.key();
+  }
+
+  {
+    GenerateFriendKeyRequest request;
+    request.set_name(names[0]);
+    GenerateFriendKeyResponse response;
+    auto status = rpcs[3]->GenerateFriendKey(nullptr, &request, &response);
+    EXPECT_TRUE(status.ok());
+    EXPECT_GT(response.key().size(), 0);
+    user4_1_key = response.key();
+  }
+
+  cout << "user1_2_key: " << user1_2_key << endl;
+  cout << "user1_3_key: " << user1_3_key << endl;
+  cout << "user1_4_key: " << user1_4_key << endl;
+  cout << "user2_1_key: " << user2_1_key << endl;
+  cout << "user3_1_key: " << user3_1_key << endl;
+  cout << "user4_1_key: " << user4_1_key << endl;
+
+  // user 1 finishes setting up users 2-4.
+  {
+    AddFriendRequest request;
+    request.set_name(names[1]);
+    request.set_key(user2_1_key);
+    AddFriendResponse response;
+    auto status = rpcs[0]->AddFriend(nullptr, &request, &response);
+    EXPECT_TRUE(status.ok());
+  }
+
+  {
+    AddFriendRequest request;
+    request.set_name(names[2]);
+    request.set_key(user3_1_key);
+    AddFriendResponse response;
+    auto status = rpcs[0]->AddFriend(nullptr, &request, &response);
+    EXPECT_TRUE(status.ok());
+  }
+
+  {
+    AddFriendRequest request;
+    request.set_name(names[3]);
+    request.set_key(user4_1_key);
+    AddFriendResponse response;
+    auto status = rpcs[0]->AddFriend(nullptr, &request, &response);
+    EXPECT_TRUE(status.ok());
+  }
+
+  // users 2-4 finish setting up user 1
+  {
+    AddFriendRequest request;
+    request.set_name(names[0]);
+    request.set_key(user1_2_key);
+    AddFriendResponse response;
+    auto status = rpcs[1]->AddFriend(nullptr, &request, &response);
+    EXPECT_TRUE(status.ok());
+  }
+
+  {
+    AddFriendRequest request;
+    request.set_name(names[0]);
+    request.set_key(user1_3_key);
+    AddFriendResponse response;
+    auto status = rpcs[2]->AddFriend(nullptr, &request, &response);
+    EXPECT_TRUE(status.ok());
+  }
+
+  {
+    AddFriendRequest request;
+    request.set_name(names[0]);
+    request.set_key(user1_4_key);
+    AddFriendResponse response;
+    auto status = rpcs[3]->AddFriend(nullptr, &request, &response);
+    EXPECT_TRUE(status.ok());
+  }
+
+  string long_message;
+  for (size_t j = 0; j < GUARANTEED_SINGLE_MESSAGE_SIZE + 1; j++) {
+    long_message += "a";
+  }
+
+  // user 1 sends message to 2-4
+  for (size_t i = 1; i < names.size(); i++) {
+    SendMessageRequest request;
+    request.set_name(names[i]);
+    request.set_message(long_message);
+    asphrdaemon::SendMessageResponse response;
+    auto status = rpcs[0]->SendMessage(nullptr, &request, &response);
+    EXPECT_TRUE(status.ok());
+  }
+
+  // execute a round
+  unordered_set<int> to_receive = {1, 2, 3};
+
+  for (auto round = 0; round < 3; round++) {
+    // first round, none!
+    for (size_t i = 0; i < names.size(); i++) {
+      ts[i]->retrieve_messages();
+      ts[i]->send_messages();
+    }
+
+    for (auto i : to_receive) {
+      GetAllMessagesRequest request;
+      GetAllMessagesResponse response;
+      auto status = rpcs[i]->GetAllMessages(nullptr, &request, &response);
+      EXPECT_TRUE(status.ok());
+      EXPECT_EQ(response.messages_size(), 0);
+    }
+
+    // second round, one!
+    for (size_t i = 0; i < names.size(); i++) {
+      ts[i]->retrieve_messages();
+      ts[i]->send_messages();
+    }
+
+    unordered_set<int> new_to_receive;
+
+    for (auto i : to_receive) {
+      GetAllMessagesRequest request;
+      GetAllMessagesResponse response;
+      auto status = rpcs[i]->GetAllMessages(nullptr, &request, &response);
+      EXPECT_TRUE(status.ok());
+      if (response.messages_size() > 0) {
+        EXPECT_EQ(response.messages_size(), 1);
+        EXPECT_EQ(response.messages(0).from(), names[0]);
+        EXPECT_EQ(response.messages(0).m().message(), long_message);
+      } else {
+        new_to_receive.insert(i);
+      }
+    }
+
+    to_receive = new_to_receive;
+  }
+
+  EXPECT_EQ(to_receive.size(), 0);
+};
+
+}  // namespace
+}  // namespace asphr::testing
---
+++ integration_tests/multiple_friends_tests/multiple_messages_test.cc
@@ -0,0 +1,162 @@
+#include "../daemon_setup.hpp"
+
+using namespace asphrdaemon;
+
+/**
+ * Things to test:
+ * - multiple friends transmitting (one person sends message to n people)
+ * - multiple friends retrieval (n people send message to one person)
+ * - long messages (that are guranteed to be chunked. Bigger than
+ *   GUARANTEED_SINGLE_MESSAGE_SIZE)
+ * - multiple friends with long messages to one person
+ * - one person sends multiple messages to another person.
+ **/
+
+namespace asphr::testing {
+namespace {
+
+class MultipleFriendsTest : public DaemonRpcTest {};
+
+TEST_F(MultipleFriendsTest, SendMultipleMessages) {
+  ResetStub();
+
+  auto crypto1 = gen_crypto();
+  auto config1 = gen_config(string(generateTempDir()), generateTempFile());
+  auto msgstore1 = gen_msgstore(config1);
+  DaemonRpc rpc1(crypto1, config1, stub_, msgstore1);
+  Transmitter t1(crypto1, config1, stub_, msgstore1);
+  auto crypto2 = gen_crypto();
+  auto config2 = gen_config(string(generateTempDir()), generateTempFile());
+  auto msgstore2 = gen_msgstore(config2);
+  DaemonRpc rpc2(crypto2, config2, stub_, msgstore2);
+  Transmitter t2(crypto2, config2, stub_, msgstore2);
+
+  {
+    RegisterUserRequest request;
+    request.set_name("user1local");
+    request.set_beta_key("asphr_magic");
+    RegisterUserResponse response;
+    rpc1.RegisterUser(nullptr, &request, &response);
+  }
+  {
+    RegisterUserRequest request;
+    request.set_name("user2local");
+    request.set_beta_key("asphr_magic");
+    RegisterUserResponse response;
+    rpc2.RegisterUser(nullptr, &request, &response);
+  }
+
+  string user1_key;
+  string user2_key;
+
+  {
+    GenerateFriendKeyRequest request;
+    request.set_name("user2");
+    GenerateFriendKeyResponse response;
+    auto status = rpc1.GenerateFriendKey(nullptr, &request, &response);
+    EXPECT_TRUE(status.ok());
+    EXPECT_GT(response.key().size(), 0);
+    user1_key = response.key();
+  }
+
+  {
+    GenerateFriendKeyRequest request;
+    request.set_name("user1");
+    GenerateFriendKeyResponse response;
+    auto status = rpc2.GenerateFriendKey(nullptr, &request, &response);
+    EXPECT_TRUE(status.ok());
+    EXPECT_GT(response.key().size(), 0);
+    user2_key = response.key();
+  }
+
+  cout << "user1_key: " << user1_key << endl;
+  cout << "user2_key: " << user2_key << endl;
+
+  {
+    AddFriendRequest request;
+    request.set_name("user2");
+    request.set_key(user2_key);
+    AddFriendResponse response;
+    auto status = rpc1.AddFriend(nullptr, &request, &response);
+    EXPECT_TRUE(status.ok());
+  }
+
+  {
+    AddFriendRequest request;
+    request.set_name("user1");
+    request.set_key(user1_key);
+    AddFriendResponse response;
+    auto status = rpc2.AddFriend(nullptr, &request, &response);
+    EXPECT_TRUE(status.ok());
+  }
+
+  {
+    SendMessageRequest request;
+    request.set_name("user2");
+    request.set_message("hello from 1 to 2");
+    asphrdaemon::SendMessageResponse response;
+    auto status = rpc1.SendMessage(nullptr, &request, &response);
+    EXPECT_TRUE(status.ok());
+  }
+
+  {
+    SendMessageRequest request;
+    request.set_name("user2");
+    request.set_message("hello from 1 to 2, again!!!! :0");
+    asphrdaemon::SendMessageResponse response;
+    auto status = rpc1.SendMessage(nullptr, &request, &response);
+    EXPECT_TRUE(status.ok());
+  }
+
+  t1.retrieve_messages();
+  t1.send_messages();
+
+  t2.retrieve_messages();
+  t2.send_messages();
+
+  {
+    GetAllMessagesRequest request;
+    GetAllMessagesResponse response;
+    auto status = rpc1.GetAllMessages(nullptr, &request, &response);
+    EXPECT_TRUE(status.ok());
+    EXPECT_EQ(response.messages_size(), 0);
+  }
+
+  {
+    GetAllMessagesRequest request;
+    GetAllMessagesResponse response;
+    auto status = rpc2.GetAllMessages(nullptr, &request, &response);
+    EXPECT_TRUE(status.ok());
+    EXPECT_EQ(response.messages_size(), 1);
+    EXPECT_EQ(response.messages(0).from(), "user1");
+    EXPECT_EQ(response.messages(0).m().message(), "hello from 1 to 2");
+  }
+
+  t1.retrieve_messages();
+  t1.send_messages();
+
+  t2.retrieve_messages();
+  t2.send_messages();
+
+  {
+    GetAllMessagesRequest request;
+    GetAllMessagesResponse response;
+    auto status = rpc1.GetAllMessages(nullptr, &request, &response);
+    EXPECT_TRUE(status.ok());
+    EXPECT_EQ(response.messages_size(), 0);
+  }
+
+  {
+    GetAllMessagesRequest request;
+    GetAllMessagesResponse response;
+    auto status = rpc2.GetAllMessages(nullptr, &request, &response);
+    EXPECT_TRUE(status.ok());
+    EXPECT_EQ(response.messages_size(), 2);
+    EXPECT_EQ(response.messages(0).from(), "user1");
+    EXPECT_EQ(response.messages(0).m().message(),
+              "hello from 1 to 2, again!!!! :0");
+  }
+};
+
+}  // namespace
+}  // namespace asphr::testing
