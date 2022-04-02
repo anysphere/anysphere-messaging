d5c42f31c937e616e3ba5d25e9f14bb0c4fb5587
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2022-04-02 16:04:56 -0400

    fix shellcheck i love shellcheck

--- wrap2.sh
+++ wrap2.sh
@@ -5,4 +5,4 @@
 # SPDX-License-Identifier: GPL-3.0-only
 #
 
-XDG_CONFIG_HOME=${HOME}/.anysphere2/data XDG_RUNTIME_DIR=${HOME}/.anysphere2/run XDG_CACHE_HOME=${HOME}/.anysphere2/cache $@
+XDG_CONFIG_HOME=${HOME}/.anysphere2/data XDG_RUNTIME_DIR=${HOME}/.anysphere2/run XDG_CACHE_HOME=${HOME}/.anysphere2/cache "$@"
--- wrap3.sh
+++ wrap3.sh
@@ -5,4 +5,4 @@
 # SPDX-License-Identifier: GPL-3.0-only
 #
 
-XDG_CONFIG_HOME=${HOME}/.anysphere3/data XDG_RUNTIME_DIR=${HOME}/.anysphere3/run XDG_CACHE_HOME=${HOME}/.anysphere3/cache $@
+XDG_CONFIG_HOME=${HOME}/.anysphere3/data XDG_RUNTIME_DIR=${HOME}/.anysphere3/run XDG_CACHE_HOME=${HOME}/.anysphere3/cache "$@"
