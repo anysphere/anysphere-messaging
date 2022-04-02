cde12d3136a826b5ea998ff21fc3e662f971bab3
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2022-04-02 15:09:03 -0400

    add wrap3 because useful

---
+++ wrap3.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#
+# Copyright 2022 Anysphere, Inc.
+# SPDX-License-Identifier: GPL-3.0-only
+#
+
+XDG_CONFIG_HOME=${HOME}/.anysphere3/data XDG_RUNTIME_DIR=${HOME}/.anysphere3/run XDG_CACHE_HOME=${HOME}/.anysphere3/cache $@
