ea91287e4c1ee715fa6a5227cecae950fb1a816a
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2022-04-03 14:01:07 -0400

    document scripts

--- wrap3.sh
+++ wrap3.sh
@@ -5,4 +5,8 @@
 # SPDX-License-Identifier: GPL-3.0-only
 #
 
+# Prepend this script to any command to have it use the .anysphere3 directory as its source of truth.
+# Useful to test two people on the same computer.
+# E.g.: ./wrap3.sh ./bazel-bin/daemon/daemon
+
 XDG_CONFIG_HOME=${HOME}/.anysphere3/data XDG_RUNTIME_DIR=${HOME}/.anysphere3/run XDG_CACHE_HOME=${HOME}/.anysphere3/cache "$@"
