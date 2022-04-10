6909c1f2a8353b4c74dd78d9d44e89bbb8770d55
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2022-04-10 15:36:13 -0400

    start daemon from gui

---
+++ gui/src/main/constants.ts
@@ -0,0 +1,41 @@
+import { systemPreferences } from "electron";
+import { exit } from "process";
+import path from "path";
+
+// this commit hash will be automatically updated by gui/package.json.
+export const RELEASE_COMMIT_HASH = "a942a89de170f21f554c2910fafc7b1b4895ab2b";
+
+export const PLIST_PATH = () => {
+  if (process.platform === "darwin" && process.env.HOME) {
+    return path.join(
+      process.env.HOME,
+      "Library",
+      "LaunchAgents",
+      "co.anysphere.anysphered.plist"
+    );
+  } else {
+    process.stderr.write("Platform not supported");
+    exit(1);
+  }
+};
+
+export const PLIST_CONTENTS = (pkgPath: string, logPath: string) => {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>Label</key>
+    <string>co.anysphere.anysphered</string>
+    <key>Program</key>
+    <string>${pkgPath}/Resources/anysphered</string>
+    <key>RunAtLoad</key>
+    <true/>
+    <key>KeepAlive</key>
+	<true/>
+    <key>StandardOutPath</key>
+    <string>${logPath}/anysphered.log</string>
+    <key>StandardErrorPath</key>
+    <string>${logPath}/anysphered.err</string>
+</dict>
+</plist>`;
+};
