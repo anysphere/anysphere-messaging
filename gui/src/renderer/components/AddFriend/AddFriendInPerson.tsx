83e915e8f6d32ac087f3396785b812f21d15f8ea
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2022-07-07 22:25:28 -0400

    add rng

--- gui/src/renderer/components/AddFriend/AddFriendInPerson.tsx
+++ gui/src/renderer/components/AddFriend/AddFriendInPerson.tsx
@@ -3,6 +3,7 @@ import { ModalType } from "../Modal";
 import { StatusProps } from "../Status";
 import { classNames } from "../../utils";
 import * as React from "react";
+import seedrandom from "seedrandom";
 
 const DEBUG_COLORS = false;
 // const DEBUG_COLORS = true;
@@ -14,59 +15,89 @@ function ProgressBar({ progress }: { progress: number }): JSX.Element {
         <div className={classNames(progress == 0 ? "text-asbrown-light" : "")}>
           1. Share stories.
         </div>
-        <div>2. Verify.</div>
-        <div>3. Finish.</div>
+        <div className={classNames(progress == 1 ? "text-asbrown-light" : "")}>
+          2. Verify.
+        </div>
+        <div className={classNames(progress == 2 ? "text-asbrown-light" : "")}>
+          3. Finish.
+        </div>
       </div>
     </div>
   );
 }
 
 export function StoryForm({
-  numberOfLines,
-  completeAll,
+  theirstory,
+  setTheirStory,
 }: {
-  numberOfLines: number;
-  completeAll: () => void;
+  theirstory: string[];
+  setTheirStory(story: string[]): void;
 }): JSX.Element {
-  let storyFromInput: String[] = [];
-  for (let i = 0; i < numberOfLines; i++) {
-    storyFromInput.push("");
-  }
-
-  function SimpleStoryLine(props: { lineNumber: number }): JSX.Element {
-    return (
-      <div className="flex flex-row gap-4">
-        <p className="text-sm text-asbrown-300">{props.lineNumber}.</p>
-        <input
-          type="text"
-          name={`Story ${props.lineNumber}`}
-          id={`Story ${props.lineNumber}`}
-          className="m-0
+  return (
+    <div className="grid grid-cols-1 gap-4">
+      {theirstory.map((line, i) => (
+        <div className="flex flex-row gap-4">
+          <p className="text-sm text-asbrown-300">{i + 1}.</p>
+          <input
+            type="text"
+            name={`Story ${i}`}
+            id={`Story ${i}`}
+            className="m-0
           block
           w-full
           border-0
           border-b-2
           border-asbrown-100 p-0 text-sm
           focus:border-asbrown-300 focus:ring-0"
-        />
-      </div>
-    );
-  }
+            onChange={(e) => {
+              setTheirStory(
+                theirstory.map((line, j) => {
+                  if (j == i) {
+                    return e.target.value;
+                  }
+                  return line;
+                })
+              );
+            }}
+            value={line}
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
 
-  // attach event listeners to the inputs
-  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
-    const lineNumber = Number(name.split(" ")[1]);
-    storyFromInput[lineNumber - 1] = value;
-  }
+async function sha256(s: string) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(s);
+  const hash = await crypto.subtle.digest("SHA-256", data);
+  return hash;
+}
+async function sha256string(s: string) {
+  const hashBuffer = await sha256(s);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("");
+  return hashHex;
+}
+
+function StoryAnimation({ seed }: { seed: string }): JSX.Element {
+  const [shaseed, setShaseed] = React.useState("");
+
+  React.useEffect(() => {
+    sha256string(seed).then((hash) => {
+      setShaseed(hash);
+    });
+  });
+
+  const rng = seedrandom(shaseed);
 
   return (
     <div className="grid grid-cols-1 gap-4">
-      {[...Array(numberOfLines)].map((_, i) => (
-        <SimpleStoryLine key={i} lineNumber={i + 1} />
-      ))}
+      {rng()}
+      {rng()}
+      {rng()}
     </div>
   );
 }
@@ -83,6 +114,7 @@ export default function AddFriendInPerson({
   const [progress, setProgress] = React.useState<number>(0);
   const [showtedious, setShowtedious] = React.useState<boolean>(false);
   const [showverify, setShowverify] = React.useState<boolean>(false);
+  const [theirstorylist, setTheirstory] = React.useState<string[]>([]);
 
   React.useEffect(() => {
     setTimeout(() => {
@@ -91,14 +123,154 @@ export default function AddFriendInPerson({
   }, [progress]);
 
   const storylist = React.useMemo(() => {
-    return story.split(". ").map((line) => {
+    let l = story.split(". ").map((line) => {
       if (line[line.length - 1] == ".") {
         return line;
       } else {
         return line + ".";
       }
     });
+    setTheirstory(new Array<string>(l.length).fill(""));
+    return l;
   }, [story]);
+
+  React.useEffect(() => {
+    if (theirstorylist.length == 0) {
+      return;
+    }
+    console.log("theirstorylist", theirstorylist);
+    let allComplete = true;
+    theirstorylist.forEach((line) => {
+      console.log("line", line);
+      if (line.length == 0) {
+        allComplete = false;
+      }
+    });
+    if (allComplete) {
+      setShowverify(true);
+    }
+  }, [theirstorylist]);
+
+  const shareStoriesComponent = (
+    <>
+      <h3
+        className={classNames(
+          "text-center font-['Lora'] text-xl",
+          DEBUG_COLORS ? "bg-yellow-700" : ""
+        )}
+      >
+        Share stories with each other.
+      </h3>
+      <h6 className="text-center text-xs text-asbrown-light">
+        These correspond to your public key, and allow you and your friend to
+        communicate privately.
+      </h6>
+      <div
+        className={classNames(
+          "grid grid-cols-2 divide-x divide-asbrown-100 self-center",
+          DEBUG_COLORS ? "bg-purple-100" : ""
+        )}
+      >
+        <div
+          className={classNames(
+            "grid items-center px-8 ",
+            DEBUG_COLORS ? "bg-green-100" : ""
+          )}
+        >
+          <div className="pb-4">
+            <h3 className="pb-8 text-center text-xs text-asbrown-light">
+              Your story.
+            </h3>
+            <div className="grid gap-4">
+              {storylist.map((feature, index) => (
+                <div className="flex flex-row gap-4" key={index}>
+                  <p className="text-sm text-asbrown-300">{index + 1}.</p>
+                  <p className="mb-[2px] text-sm">{feature}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div
+          className={classNames(
+            "grid items-center px-8 ",
+            DEBUG_COLORS ? "bg-red-100" : ""
+          )}
+        >
+          <div className="pb-4">
+            <h3 className="pb-8 text-center text-xs text-asbrown-light">
+              Their story.
+            </h3>
+            <StoryForm
+              theirstory={theirstorylist}
+              setTheirStory={(story) => setTheirstory(story)}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="grid items-center justify-center text-center">
+        <button
+          className={classNames(
+            "unselectable rounded-lg bg-asbrown-100 px-3 py-1 text-asbrown-light",
+            showverify ? "" : "disabled cursor-not-allowed opacity-20"
+          )}
+          onClick={() => {
+            console.log("next step");
+            if (showverify) {
+              setProgress(1);
+            }
+          }}
+        >
+          Verify
+        </button>
+      </div>
+      <p
+        className={classNames(
+          "self-center px-20 text-center text-xs transition",
+          showtedious ? "text-asbrown-200" : "text-transparent"
+        )}
+      >
+        Tedious? We know, complete privacy is work. But trust us, it is worth
+        it.
+      </p>
+    </>
+  );
+
+  const verifyStoriesComponent = (
+    <>
+      <h3
+        className={classNames(
+          "text-center font-['Lora'] text-xl",
+          DEBUG_COLORS ? "bg-yellow-700" : ""
+        )}
+      >
+        Are the animations identical?
+      </h3>
+      <div>
+        <StoryAnimation seed={story + theirstorylist.join("")} />
+      </div>
+      <div className="grid items-center justify-center text-center">
+        <button
+          className={classNames(
+            "unselectable rounded-lg bg-asbrown-100 px-3 py-1 text-asbrown-light"
+          )}
+        >
+          Yes, they are identical.
+        </button>
+        <button
+          className="unselectable text-xs text-asbrown-200"
+          onClick={() => setProgress(0)}
+        >
+          ‹ No, take me back.
+        </button>
+      </div>
+    </>
+  );
+  const finishComponent = (
+    <>
+      <div>unimplemented</div>
+    </>
+  );
 
   return (
     <div className="grid h-full w-full items-center">
@@ -109,85 +281,18 @@ export default function AddFriendInPerson({
         )}
       >
         <ProgressBar progress={progress} />
-        <h3
-          className={classNames(
-            "text-center font-['Lora'] text-xl",
-            DEBUG_COLORS ? "bg-yellow-700" : ""
-          )}
-        >
-          Share stories with each other.
-        </h3>
-        <h6 className="text-center text-xs text-asbrown-light">
-          These correspond to your public key, and allow you and your friend to
-          communicate privately.
-        </h6>
-        <div
-          className={classNames(
-            "grid grid-cols-2 divide-x divide-asbrown-100 self-center",
-            DEBUG_COLORS ? "bg-purple-100" : ""
-          )}
-        >
-          <div
-            className={classNames(
-              "grid items-center px-8 ",
-              DEBUG_COLORS ? "bg-green-100" : ""
-            )}
-          >
-            <div className="pb-4">
-              <h3 className="pb-8 text-center text-xs text-asbrown-light">
-                Your story.
-              </h3>
-              <div className="grid gap-4">
-                {storylist.map((feature, index) => (
-                  <div className="flex flex-row gap-4" key={index}>
-                    <p className="text-sm text-asbrown-300">{index + 1}.</p>
-                    <p className="mb-[2px] text-sm">{feature}</p>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-          <div
-            className={classNames(
-              "grid items-center px-8 ",
-              DEBUG_COLORS ? "bg-red-100" : ""
-            )}
-          >
-            <div className="pb-4">
-              <h3 className="pb-8 text-center text-xs text-asbrown-light">
-                Their story.
-              </h3>
-              <StoryForm
-                numberOfLines={storylist.length}
-                completeAll={() => setShowverify(true)}
-              />
-            </div>
-          </div>
-        </div>
-        <p
-          className={classNames(
-            "self-center px-20 pt-5 text-center text-xs transition",
-            showtedious ? "text-asbrown-200" : "text-transparent",
-            showverify ? "hidden" : ""
-          )}
-        >
-          Tedious? We know, complete privacy is work. But trust us, it is worth
-          it.
-        </p>
-        <div className="grid items-center justify-center text-center">
-          <button
-            className={classNames(
-              "unselectable rounded-lg bg-asbrown-100 px-3 py-1 text-asbrown-light",
-              showverify ? "" : "hidden"
-            )}
-            onClick={() => {
-              console.log("next step");
-              setShowverify(false);
-            }}
-          >
-            Verify
-          </button>
-        </div>
+        {(() => {
+          switch (progress) {
+            case 0:
+              return shareStoriesComponent;
+            case 1:
+              return verifyStoriesComponent;
+            case 2:
+              return finishComponent;
+            default:
+              console.log("ERRORRRRRR");
+          }
+        })()}
       </div>
     </div>
   );
