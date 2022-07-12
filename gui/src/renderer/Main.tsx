0ce5826883bcc9b4dd7956091bd3bf0d19970e34
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2022-07-11 23:52:21 -0400

    remove legacy friend

--- gui/src/renderer/Main.tsx
+++ gui/src/renderer/Main.tsx
@@ -8,7 +8,6 @@ import * as React from "react";
 import MessageList from "./components/MessageList";
 import Read from "./components/Read";
 import Write from "./components/Write";
-import { InitFriendModal } from "./components/FriendsModal";
 import { RegisterModal } from "./components/RegisterModal";
 import { Message } from "../types";
 import { Tab, TabType, TabContainer, useTabs } from "./components/Tabs";
