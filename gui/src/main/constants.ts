aadcb532024a158a913209c669671af88dd65c44
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2022-08-11 16:14:50 -0400

    fix eslint errors in constants.ts

--- gui/src/main/constants.ts
+++ gui/src/main/constants.ts
@@ -5,10 +5,10 @@ import path from "path";
 // this commit hash will be automatically updated by gui/package.json.
 export const RELEASE_COMMIT_HASH = "cc82ac0e0711d411387d59c98a5deb9adeca155d";
 
-export const PLIST_PATH = () => {
-  if (process.platform === "darwin" && process.env.HOME) {
+export const PLIST_PATH = (): string => {
+  if (process.platform === "darwin" && process.env["HOME"] != null) {
     return path.join(
-      process.env.HOME,
+      process.env["HOME"],
       "Library",
       "LaunchAgents",
       "co.anysphere.anysphered.plist"
@@ -19,7 +19,7 @@ export const PLIST_PATH = () => {
   }
 };
 
-export const PLIST_CONTENTS = (pkgPath: string, logPath: string) => {
+export const PLIST_CONTENTS = (pkgPath: string, logPath: string): string => {
   return `<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
@@ -31,11 +31,31 @@ export const PLIST_CONTENTS = (pkgPath: string, logPath: string) => {
     <key>RunAtLoad</key>
     <true/>
     <key>KeepAlive</key>
-	<true/>
+    <true/>
     <key>StandardOutPath</key>
     <string>${logPath}/anysphered.log</string>
     <key>StandardErrorPath</key>
     <string>${logPath}/anysphered.err</string>
 </dict>
 </plist>`;
+};
+
+export const SYSTEMD_UNIT_CONTENTS = (
+  pkgPath: string,
+  logPath: string
+): string => {
+  return `[Unit]
+Description=Anysphere daemon
+After=network.target
+
+[Service]
+Type=simple
+User=${process.env["USER"]}
+WorkingDirectory=${pkgPath}
+ExecStart=${pkgPath}/bin/anysphered
+Restart=on-failure
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target`;
 };
